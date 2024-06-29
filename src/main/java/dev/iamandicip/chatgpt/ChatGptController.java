9ef2e96467ad4d7000c4b7034b8c83ca41df2e44
Ciprian Iamandi <ciprian.iamandi@gmail.com>
2024-06-29 20:18:25 +0200

    initial commit

---
+++ src/main/java/dev/iamandicip/chatgpt/ChatGptController.java
@@ -0,0 +1,52 @@
+package dev.iamandicip.chatgpt;
+
+import io.github.wimdeblauwe.htmx.spring.boot.mvc.HtmxResponse;
+import io.github.wimdeblauwe.htmx.spring.boot.mvc.HxRequest;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
+import org.springframework.ai.chat.memory.InMemoryChatMemory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@CrossOrigin
+@Slf4j
+public class ChatGptController {
+
+    private final ChatClient chatClient;
+
+    public ChatGptController(ChatClient.Builder builder) {
+        this.chatClient = builder
+                .defaultAdvisors(new MessageChatMemoryAdvisor(new InMemoryChatMemory()))
+                .build();
+    }
+
+    @GetMapping("")
+    public String home() {
+        return "index";
+    }
+
+    @HxRequest
+    @PostMapping("/api/chat")
+    public HtmxResponse generate(@RequestParam String message, Model model) {
+        log.debug("User message: {}", message);
+
+        String response = chatClient.prompt()
+                .user(message)
+                .call()
+                .content();
+
+        model.addAttribute("response", response);
+        model.addAttribute("message", message);
+
+        return HtmxResponse.builder()
+                .view("response :: responseFragment")
+                .view("recent-message-list :: messageFragment")
+                .build();
+    }
+}
---
+++ src/test/java/dev/iamandicip/chatgpt/ApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.iamandicip.chatgpt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
